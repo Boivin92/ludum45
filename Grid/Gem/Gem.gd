8a5f01f29a2eb4618898ade074912b226c6d231b
xenoexplorator <cedric.pinard@gmail.com>
2019-10-05 09:51:27 -0400

    Détection des match de gemmes

--- Grid/Gem/Gem.gd
+++ Grid/Gem/Gem.gd
@@ -4,6 +4,9 @@ extends TextureRect
 
 export(Vector2) var coords setget set_coords, get_coords
 
+enum Types { Ruby, Sapphire, Emerald }
+export(Types) var type
+
 signal gem_selected(gem)
 
 
@@ -16,8 +19,33 @@ func get_coords() -> Vector2:
 
 
 func set_highlight(highlighted: bool) -> void:
-	$Highlight.visible = highlighted
+	$Offset/Highlight.visible = highlighted
+
+
+func check_matches() -> bool:
+	var neighbour_up = $Offset/Area2D/RayCastUp.get_collider()
+	var neighbour_down = $Offset/Area2D/RayCastDown.get_collider()
+	if neighbour_up != null and neighbour_down != null:
+		var gem_up = neighbour_up.find_parent("Gem*")
+		var gem_down = neighbour_down.find_parent("Gem*")
+		if gem_up.type == self.type and gem_down.type == self.type:
+			return true
+
+	var neighbour_left = $Offset/Area2D/RayCastLeft.get_collider()
+	var neighbour_right = $Offset/Area2D/RayCastRight.get_collider()
+	if neighbour_left != null and neighbour_right != null:
+		var gem_left = neighbour_left.find_parent("Gem*")
+		var gem_right = neighbour_right.find_parent("Gem*")
+		if gem_left.type == self.type and gem_right.type == self.type:
+			return true
+
+	return false
+
 
 func _gui_input(event: InputEvent) -> void:
 	if event.is_action_released("ui_select"):
 		emit_signal("gem_selected", self)
+
+
+#func _ready() -> void:
+#	$Offset/Area2D/RayCastUp.add_exception(self)
--- Grid/Grid.gd
+++ Grid/Grid.gd
@@ -5,23 +5,44 @@ export(int, 3, 12) var width = 3
 
 var selected = null
 
-func is_adjacent(coord1: Vector2, coord2: Vector2) -> bool:
+func find_gem(coord: Vector2):
+	for gem in get_children():
+		if gem.coords == coord:
+			return gem
+	return null
+
+func can_swap(coord1: Vector2, coord2: Vector2) -> bool:
 	if selected == null:
 		return false
 	var delta = (coord1 - coord2).abs()
 	var distance_ortho = delta.x + delta.y
 	return distance_ortho == 1
 
+
+func try_swap(gem1, gem2) -> void:
+	if not can_swap(gem1.coords, gem2.coords):
+		return
+	var temp = gem1.coords
+	gem1.coords = gem2.coords
+	gem2.coords = temp
+	yield(get_tree(), "idle_frame")
+	match_gems()
+
+
+func match_gems():
+	for x in range(width):
+		for y in range(height):
+			var gem = find_gem(Vector2(x, y))
+			if gem.check_matches():
+				print("You win! (%d, %d)" % [x, y])
+
+
 func _on_gem_selected(gem) -> void:
+	gem.set_highlight(selected == null)
 	if selected == null:
-		selected = gem
 		gem.set_highlight(true)
-	elif is_adjacent(gem.get_coords(), selected.get_coords()):
-		selected.set_highlight(false)
-		var temp = selected.get_coords()
-		selected.set_coords(gem.get_coords())
-		gem.set_coords(temp)
-		selected = null
+		selected = gem
 	else:
+		selected.set_highlight(false)
+		try_swap(selected, gem)
 		selected = null
-		gem.set_highlight(false)
