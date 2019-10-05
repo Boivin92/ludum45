976aef0a496774655dd04a516638bcf88a0e510e
Boivin92 <boivin.92@gmail.com>
2019-10-05 14:27:05 -0400

    QOL

--- testScenes/testFocusPoint.gd
+++ testScenes/testFocusPoint.gd
@@ -10,11 +10,14 @@ var step : int = 0
 export(Array, String, MULTILINE) var poems = []
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("debug_play_transition"):
 		$AnimationPlayer.play("transition" + str(step))
 
 func start_new_game():
 	emit_signal("transition_completed")
+
+func transition_to_next():
+	$AnimationPlayer.play("transition" + str(step))
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	$DialogBox.text = poems[step]
