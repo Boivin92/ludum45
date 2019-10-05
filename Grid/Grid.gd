a1a2992e936e7a3f81d95d250f313d0a8ed36fc5
xenoexplorator <cedric.pinard@gmail.com>
2019-10-04 23:49:43 -0400

    Logique de swap fonctionnelle

--- Grid/Grid.gd
+++ Grid/Grid.gd
@@ -5,19 +5,23 @@ export(int, 3, 12) var width = 3
 
 var selected = null
 
-func is_adjacent(coord: Vector2) -> bool:
+func is_adjacent(coord1: Vector2, coord2: Vector2) -> bool:
 	if selected == null:
 		return false
-	var delta = (coord - selected).abs()
+	var delta = (coord1 - coord2).abs()
 	var distance_ortho = delta.x + delta.y
 	return distance_ortho == 1
 
 func _on_gem_selected(gem) -> void:
 	if selected == null:
-		selected = gem.get_coords()
+		selected = gem
 		gem.set_highlight(true)
-	elif is_adjacent(gem.get_coords()):
-		print("swap!")
+	elif is_adjacent(gem.get_coords(), selected.get_coords()):
+		selected.set_highlight(false)
+		var temp = selected.get_coords()
+		selected.set_coords(gem.get_coords())
+		gem.set_coords(temp)
+		selected = null
 	else:
 		selected = null
 		gem.set_highlight(false)
