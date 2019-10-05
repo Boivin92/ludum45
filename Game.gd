f33dbae273b5c218f3a66a23c76dd86c497b30f0
Ellias, Mélanie <Melanie.Ellias@ia.ca>
2019-10-04 23:02:32 -0400

    aller-retour page accueil et jeu

---
+++ Game.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+
+
+
+
+func _on_RetourAccueil_pressed() -> void:
+	get_tree().change_scene("res://EcranAccueil.tscn")
+	pass # Replace with function body.
